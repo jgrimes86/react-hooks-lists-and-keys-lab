47790b77881ddcb72f2f65ceb619be6f6e29f060
Jim Grimes <jimgrimes86@gmail.com>
2023-10-02 11:47:49 -0400

    completed lab

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  let id=0;
+  const techList = technologies.map(tech => {
+    ++id;
+    return <span key={name+id}>{tech}</span>
+  })
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {techList}
       </div>
     </div>
   );
